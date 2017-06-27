dde6a12f88b53dbebb9f4f9b35af460509549143
Collin Moore <collin.moore96@gmail.com>
2017-06-26 19:30:45 -0600

    password remove

--- PasswordKeeper/src/app/password-display-card/password-display-card.component.ts
+++ PasswordKeeper/src/app/password-display-card/password-display-card.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Password } from "app/models/password.model";
 import { trigger, state, style, transition, animate } from "@angular/animations";
 
+import * as firebase from 'firebase/app';
+import { MdSnackBar } from "@angular/material";
+
 @Component({
   selector: 'app-password-display-card',
   templateUrl: './password-display-card.component.html',
@@ -19,15 +22,26 @@ import { trigger, state, style, transition, animate } from "@angular/animations"
 export class PasswordDisplayCardComponent implements OnInit {
 
   @Input() password: Password;
+  @Input() firebasePath: string;
   isExpanded = false;
 
   get showPasswordState(): string {
     return this.isExpanded ? 'expanded' : 'collapsed';
   }
 
-  constructor() { }
+  constructor(private snackBar: MdSnackBar) { }
 
   ngOnInit() {
   }
 
+  edit(): void {
+    
+  }
+
+  remove(): void {
+    firebase.database().ref(this.firebasePath).child(this.password.$key).remove();
+    this.snackBar.open("Password removed", "Dismiss", {
+      duration: 3000,
+    });
+  }
 }
